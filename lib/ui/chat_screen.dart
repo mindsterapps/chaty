988545db18c1b048cf33cb7d8a870aaa4df1ed52
Aswin T <aswin.t@mindster.in>
2025-03-14 19:05:30 +0530

    scoll detection

--- lib/ui/chat_screen.dart
+++ lib/ui/chat_screen.dart
@@ -128,7 +128,7 @@ class _ChatScreenState extends State<ChatScreen> {
   void _onScroll() {
     // Detect when user scrolls past 50% of the list height
     if (_scrollController.position.pixels <
-            _scrollController.position.maxScrollExtent / 9 &&
+            _scrollController.position.maxScrollExtent / 05 &&
         !_isLoadingMore &&
         _hasMoreMessages) {
       _loadMoreMessages();
