e78fbe6a24d1f9adde85c382bff5731a7b00c776
aswintbbc <mindsterapps@mindster.in>
2025-06-02 17:17:35 +0530

    Add typing status management to message sending in ChatService

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -57,9 +57,11 @@ class ChatService {
       'lastMessageTime': FieldValue.serverTimestamp(),
       'lastMessageSender': message.senderId, // Store sender's ID
       'users': [message.senderId, message.receiverId],
-      'unreadMessageCount': {
-        '${message.receiverId}': FieldValue.increment(1)
-      }, // ✅ Increase unread count
+      'unreadMessageCount': {'${message.receiverId}': FieldValue.increment(1)},
+      'typingStatus': {
+        '${message.senderId}': true,
+        '${message.receiverId}': false,
+      },
     }, SetOptions(merge: true));
   }
 
@@ -200,11 +202,7 @@ class ChatService {
           'users': [message.senderId, message.receiverId],
           'unreadMessageCount': {
             '${message.receiverId}': FieldValue.increment(1)
-          }, // ✅ Increase unread count
-          'typingStatus': {
-            '${message.senderId}': true,
-            '${message.receiverId}': false,
-          }, // ✅ Increase unread count
+          },
         }, SetOptions(merge: true));
         // Delete the message from Firestore
         await messageRef.update({
